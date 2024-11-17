81b655c3c39b146725b95c38c3cde06777739ef4
Sergio Ungueti <ungar727@gmail.com>
2024-11-17 18:56:00 +0100

    canem-intro gestion animales y fondo landing page

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Animal
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -11,3 +11,13 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email', 'full_name', 'phone_number', 'address', 'birth_date', 'profile_picture')
+
+class AnimalForm(forms.ModelForm):
+    class Meta:
+        model = Animal
+        fields = ['name', 'age', 'species', 'description', 'image', 'adoption_status']
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 4}),
+        }
+
+#, 'shelter'
