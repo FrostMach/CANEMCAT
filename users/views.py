3b4d11340092f390fe809b79d5dfe6c64add3d8d
user <email@email.com>
2024-11-28 19:41:52 +0100

    canem-solicitud-encuesta-adopcion registro, activar cuenta

--- users/views.py
+++ users/views.py
@@ -1,24 +1,58 @@
-from django.shortcuts import render, redirect,redirect
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,PasswordResetForm
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.core.mail import send_mail
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import PasswordResetForm
 from .models import CustomUser, Wishlist
 from django.contrib.auth import login, authenticate, logout, get_user_model
 from .forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib import messages
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+import smtplib
 from django.http import HttpResponse
 import requests
+
 
 def landing_page(request):
     return render(request, 'landing_page.html')
 
 #USUARIOS
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+class ProfileView(generic.DetailView):
+    model = CustomUser
+    template_name = 'profile.html'
+
+class ProfileUpdateView(generic.UpdateView):
+    model = CustomUser
+    form_class = CustomUserChangeForm
+    template_name = 'profile_update.html'
+    success_url = reverse_lazy('profile')
+    
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('landing_page')  # Redirigir a la página principal o a la página de perfil
+        else:
+            # Si el inicio de sesión es incorrecto, muestra un mensaje de error
+            return render(request, 'registration/login.html', {'error': 'Correo o contraseña incorrectos'})
+
+    return render(request, 'registration/login.html')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -28,9 +62,9 @@ def signup(request):
             user.is_active = False  # El usuario no estará activo hasta que confirme su email
             user.save()
 
-            # Enviar correo de confirmación
+            # # Enviar correo de confirmación
             token = default_token_generator.make_token(user)
-            uid = urlsafe_base64_encode(str(user.pk).encode())
+            uid = urlsafe_base64_encode(user.pk.to_bytes())
             current_site = get_current_site(request)
             mail_subject = 'Confirma tu correo electrónico'
             message = render_to_string('registration/activation_email.html', {
@@ -39,12 +73,47 @@ def signup(request):
                 'uid': uid,
                 'token': token,
             })
-            send_mail(mail_subject, message, 'pruebaconsmtp46@gmail.com', [user.email])
+            send_mail(user.email,mail_subject, message)
 
-            return redirect('registration/email_confirmation.html')
+            return render(request,'registration/email_confirmation.html')
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+def send_mail(to, subject, body, from_mail="pruebasconsmtp46@gmail.com", password="mmxl tlnu lxpt rvjr"):
+    from email.mime.text import MIMEText
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = from_mail
+    msg['To'] = to
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login(from_mail, password)
+    s.sendmail(from_mail, [to], msg.as_string())
+    s.quit()
+
+def password_reset(request):
+    if request.method == 'POST':
+        form = PasswordResetForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            users = CustomUser.objects.filter(email=email)
+            for user in users:
+                token = default_token_generator.make_token(user)
+                uid = urlsafe_base64_encode(str(user.pk).encode())
+                current_site = get_current_site(request)
+                mail_subject = 'Recupera tu contraseña'
+                message = render_to_string('registration/password_reset_email.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': uid,
+                    'token': token,
+                })
+                send_mail(mail_subject, message, 'pruebasconsmtp46@gmail.com', [email])
+            return redirect('password_reset_confirm')
+    else:
+        form = PasswordResetForm()
+    return render(request, 'registration/password_reset.html', {'form': form})
 
 def user_registration_view(request):
     if request.method == "POST":
@@ -78,81 +147,7 @@ def activate(request, uidb64, token):
         messages.error(request, "El enlace de activación es inválido.")
     return redirect('login')
 
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('landing_page')  # Redirigir al home u otra página
-    else:
-        form = AuthenticationForm()
-    return render(request, 'registration/login.html', {'form': form})
 
-def password_reset(request):
-    if request.method == 'POST':
-        form = PasswordResetForm(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data.get('email')
-            users = CustomUser.objects.filter(email=email)
-            for user in users:
-                token = default_token_generator.make_token(user)
-                uid = urlsafe_base64_encode(str(user.pk).encode())
-                current_site = get_current_site(request)
-                mail_subject = 'Recupera tu contraseña'
-                message = render_to_string('registration/password_reset_email.html', {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': uid,
-                    'token': token,
-                })
-                send_mail(mail_subject, message, 'pruebasconsmtp46@gmail.com', [email])
-            return redirect('password_reset_confirm')
-    else:
-        form = PasswordResetForm()
-    return render(request, 'registration/password_reset.html', {'form': form})
-
-class ProfileView(generic.DetailView):
-    model = CustomUser
-    template_name = 'profile.html'
-
-class ProfileUpdateView(generic.UpdateView):
-    model = CustomUser
-    form_class = CustomUserChangeForm
-    template_name = 'profile_update.html'
-    success_url = reverse_lazy('profile')
-    
-
-def login_view(request):
-    if request.method == "POST":
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = authenticate(request, email=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('landing_page')  # Redirigir a la página principal o a la página de perfil
-        else:
-            # Si el inicio de sesión es incorrecto, muestra un mensaje de error
-            return render(request, 'registration/login.html', {'error': 'Correo o contraseña incorrectos'})
-
-    return render(request, 'registration/login.html')
-
-
-# def signup_view(request):
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)  # Iniciar sesión automáticamente
-#             return redirect('landing_page')  # Redirigir a la página principal o perfil
-#     else:
-#         form = UserCreationForm()
-
-#     return render(request, 'signup.html', {'form': form})
 
 def logout_view(request):
     logout(request)
